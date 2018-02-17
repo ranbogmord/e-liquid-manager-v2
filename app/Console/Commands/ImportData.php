10262c89bf5a8c8ea816e9766d02a51f9cba1056
John Eriksson <john@provideit.se>
2018-02-17 14:26:06 +0100

    Work on import command

--- app/Console/Commands/ImportData.php
+++ app/Console/Commands/ImportData.php
@@ -2,7 +2,11 @@
 
 namespace App\Console\Commands;
 
+use App\Comment;
+use App\Flavour;
+use App\Liquid;
 use App\User;
+use App\Vendor;
 use Carbon\Carbon;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
@@ -37,6 +41,9 @@ class ImportData extends Command
     /**
      * Execute the console command.
      *
+     * ID is mapped by ["field"]["$oid"], usually _id
+     * Dates are mapped by ["fieldName"]["$date"]
+     *
      * @return mixed
      */
     public function handle()
@@ -59,11 +66,110 @@ class ImportData extends Command
                 $user->username = $oldUser['username'];
                 $user->email = $oldUser['email'];
                 $user->role = $oldUser['role'] ?? "user";
-                $user->password = Uuid::uuid4()->toString();
+                $user->password = $oldUser["password"];
                 $user->created_at = new Carbon($oldUser['createdAt']['$date'] ?? null);
                 $user->updated_at = new Carbon($oldUser['updatedAt']['$date'] ?? null);
 
+                if (User::whereEmail($user->email)->first()) {
+                    $user->email = Uuid::uuid4()->toString() . '@example.com';
+                }
+
                 $user->save();
+            }
+        }
+
+        if (!empty($data['vendors'])) {
+            foreach ($data['vendors'] as $oldVendor) {
+                $vendor = new Vendor();
+                $vendor->old_id = $oldVendor['_id']['$oid'];
+                $vendor->name = $oldVendor['name'];
+                $vendor->abbr = $oldVendor['abbr'] ?? "";
+                $vendor->created_at = new Carbon($oldVendor['createdAt']['$date'] ?? null);
+                $vendor->updated_at = new Carbon($oldVendor['updatedAt']['$date'] ?? null);
+
+                if ($user = User::whereOldId($oldVendor['addedBy']['$oid'])->first()) {
+                    $vendor->author_id = $user->id;
+                }
+
+                $vendor->save();
+            }
+        }
+
+        if (!empty($data['flavours'])) {
+            foreach ($data['flavours'] as $oldFlavour) {
+                $flavour = new Flavour();
+                $flavour->old_id = $oldFlavour['_id']['$oid'];
+                $flavour->name = $oldFlavour['name'];
+                $flavour->is_vg = $oldFlavour['isVg'];
+                $flavour->base_percent = $oldFlavour['basePercent'];
+
+                if ($vendor = Vendor::whereOldId($oldFlavour['vendor']['$oid'])->first()) {
+                    $flavour->vendor_id = $vendor->id;
+                }
+
+                if ($user = User::whereOldId($oldFlavour['addedBy']['$oid'])->first()) {
+                    $flavour->author_id = $user->id;
+                }
+
+                $flavour->created_at = new Carbon($oldFlavour["createdAt"]['$date'] ?? null);
+                $flavour->updated_at = new Carbon($oldFlavour["updatedAt"]['$date'] ?? null);
+
+                $flavour->save();
+            }
+        }
+
+        if (!empty($data['liquids'])) {
+            foreach ($data['liquids'] as $oldLiquid) {
+                $liquid = new Liquid();
+                $liquid->old_id = $oldLiquid['_id']['$oid'];
+
+                $liquid->name = $oldLiquid['name'];
+                $liquid->base_nic_strength = $oldLiquid['base']['nicStrength'];
+                $liquid->target_pg_percentage = $oldLiquid['target']['pgPercent'];
+                $liquid->target_vg_percentage = $oldLiquid['target']['vgPercent'];
+                $liquid->target_nic_strength = $oldLiquid['target']['nicStrength'];
+
+                if ($user = User::whereOldId($oldLiquid['author']['$oid'])->first()) {
+                    $liquid->author_id = $user->id;
+                }
+
+                $liquid->created_at = new Carbon($oldLiquid['createdAt']['$date'] ?? null);
+                $liquid->updated_at = new Carbon($oldLiquid['updatedAt']['$date'] ?? null);
+
+                $liquid->save();
+
+                $flavours = [];
+                foreach ($oldLiquid['flavours'] as $oldFlavourRef) {
+                    if ($flavour = Flavour::whereOldId($oldFlavourRef['flavour']['_id'])->first()) {
+                        $flavours[$flavour->id] = [
+                            'percent' => $oldFlavourRef['perc']
+                        ];
+                    }
+                }
+
+                $liquid->flavours()->sync($flavours);
+            }
+        }
+
+        if (!empty($data['comments'])) {
+            foreach ($data['comments'] as $oldComment) {
+                $comment = new Comment();
+                $comment->old_id = $oldComment['_id']['$oid'];
+
+                $comment->comment = $oldComment['comment'];
+
+                if ($user = User::whereOldId($oldComment['author']['$oid'])->first()) {
+                    $comment->author_id = $user->id;
+                }
+
+                if ($liquid = Liquid::whereOldId($oldComment['liquid']['$oid'])->first()) {
+                    $comment->liquid_id = $liquid->id;
+                }
+
+                $comment->created_at = $oldComment['createdAt']['$date'];
+                $comment->updated_at = $oldComment['updatedAt']['$date'];
+
+                $comment->save();
             }
         }
     }
